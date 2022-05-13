939f714773417fa1a457e6f0de87f97b244fb9e3
Daniel Bergløv <d.bergloev@mailbox.org>
2022-05-13 11:06:14 +0200

    Bump dependency versions

--- stub.php
+++ stub.php
@@ -1,10 +1,16 @@
 <?php
 
 if (!defined('\im\IMPHP_BASE')) {
-    echo "Could not find imphp/base"; exit(1);
+    throw new Exception("Could not find imphp/base");
+
+} else if (version_compare(\im\IMPHP_BASE, ($v = '1.1.0'), '<')) {
+    throw new Exception("The package imphp/base-". \im\IMPHP_BASE ." is to old. At least $v is requried");
 
 } else if (!defined('\im\IMPHP_HTTP')) {
-    echo "Could not find imphp/http"; exit(1);
+    throw new Exception("Could not find imphp/http");
+
+} else if (version_compare(\im\IMPHP_HTTP, ($v = '1.2.1'), '<')) {
+    throw new Exception("The package imphp/http-". \im\IMPHP_HTTP ." is to old. At least $v is requried");
 }
 
 require "static.php";
